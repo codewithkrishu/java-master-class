cded8b006181f7588ed4753c194c24ea3093216b
codewithkrishu <krishukashyap2003@gmail.com>
2026-08-30 11:52:55 +0530

    Add Problem13_RawTypes.java with raw types example

---
+++ Level-12-Generics-Practice/13 — Problem13_RawTypes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Problem13_RawTypes {
+
+    public static void main(String[] args) {
+
+        ArrayList names = new ArrayList();
+
+        names.add("Krishu");
+        names.add(78);
+        names.add(56.5);
+
+        System.out.println(names);
+    }
+}
